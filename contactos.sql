49159101d553ca38f00e99853c132769021dd0c9
Citlali Hernandez <1722110136@utectulancingo.edu.mx>
2023-10-19 16:24:21 +0000

    update

--- contactos.sql
+++ contactos.sql
@@ -1,11 +1,11 @@
 CREATE TABLE contactos (
-    email TEXT PRIMARY KEY,
+    email VARCHAR PRIMARY KEY,
     nombre TEXT,
     telefono TEXT
 );
 
-INSERT INTO contactos (email, nombre, telefonos)
+INSERT INTO contactos (email, nombre, telefono)
 VALUES ("juan@example.com", "Juan Pérez", "555-123-4567");
 
-INSERT INTO contactos (emial, nombre, telefono)
+INSERT INTO contactos (email, nombre, telefono)
 VALUES ("maria@example.com", "María García", "555-678-9012");
